0366748ca24337b5d90441c4cc633c05c21f7260
Adam Kazai <kazai.adam@gmail.com>
2024-06-21 16:30:12 +0200

    create the base class for the catalog items tag

---
+++ src/braze/tags/catalogItems.ts
@@ -0,0 +1,12 @@
+import Context from "../../context/context";
+import TagToken from "../../parser/tag-token";
+import ITagImplOptions from "../../template/tag/itag-impl-options";
+
+export default <ITagImplOptions>{
+    parse: function (tagToken: TagToken) {
+
+    },
+    render: async function (ctx: Context) {
+
+    }
+}
